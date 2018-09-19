f33803110b3295c23cd44c558fc3177409015548
ben1iu <422024380@qq.com>
2018-09-19 23:37:03 +0800

    modify function

--- function.php
+++ function.php
@@ -126,4 +126,31 @@ if ($type != 'all') {
 }
 	return $text;
 }
+/**
+* 获取客户端IP地址
+*/
+function get_client_ip($type = 0)
+{
+	$type = $type ? 1 : 0;
+	static $ip = null;
+	if ($ip !== null) {
+		return $ip[$type];
+	}
+	if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+		$arr = explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
+		$pos = array_search('unknown', $arr);
+		if (false !== $pos) {
+			unset($arr[$pos]);
+		}
+		$ip = trim($arr[0]);
+	} elseif (isset($_SERVER['HTTP_CLIENT_IP'])) {
+		$ip = $_SERVER['HTTP_CLIENT_IP'];
+	} elseif (isset($_SERVER['REMOTE_ADDR'])) {
+		$ip = $_SERVER['REMOTE_ADDR'];
+	}
+	// IP地址合法验证
+	$long = sprintf("%u", ip2long($ip));
+	$ip = $long ? array($ip, $long) : array('127.0.0.1', 0);
+	return $ip[$type];
+}
 
